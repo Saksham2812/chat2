3e2197b43de9d8c428239b75428812d6a0821d42
Saksham2812 <94033877+Saksham2812@users.noreply.github.com>
2021-11-25 19:57:37 +0530

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,5 @@
+function Start()
+{
+ 
+    window.location.replace("Kwitter_room.html");
+}
---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDZmMWlcbME277sPsyMj-EIpPQpxRWs8Dc",
+  authDomain: "chatwebapp-5a470.firebaseapp.com",
+  projectId: "chatwebapp-5a470",
+  storageBucket: "chatwebapp-5a470.appspot.com",
+  messagingSenderId: "919531744897",
+  appId: "1:919531744897:web:9ebb76daae0d42e362a780"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
